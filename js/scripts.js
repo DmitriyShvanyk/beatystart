037663994196f72dfe3898f88a3085dae3ac1a73
DmitriyShvanyk <soledar1984@gmail.com>
2019-06-15 07:29:50 +0300

    Init commet

---
+++ js/scripts.js
@@ -0,0 +1,130 @@
+; (function () {
+
+    "use strict";
+
+    /*
+		* -------------------------------------------------------
+		*  CAROUSEL LESSONS
+		* -------------------------------------------------------
+	*/
+   
+    function owlInitialize() {
+        if ($(window).width() < 991) {
+            $('.carousel-lessons').owlCarousel({
+                loop: true,
+                margin: 0,
+                dots: false,
+                nav: false,
+                lazyLoad: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                        center: true,
+                        stagePadding: 65,
+                        dots: true
+                    },
+                    767: {
+                        items: 3,
+                        center: true,
+                        stagePadding: 85,
+                        dots: true
+                    },
+                    991: {
+                        items: 3,
+                        center: true,
+                        stagePadding: 105,
+                        dots: true
+                    }
+                }
+            });
+        } else {
+            $('.carousel-lessons').owlCarousel('destroy');
+        }
+    }
+    $(document).ready(function (e) {
+        owlInitialize();
+    });
+    $(window).resize(function () {
+        owlInitialize();
+    });
+
+
+    /*
+		* -------------------------------------------------------
+		*  PARALLAX
+		* -------------------------------------------------------
+	*/
+    if ($('.js-parallax').length > 0) {
+        $('.js-parallax').paroller();
+    }
+
+
+	/*
+		* -------------------------------------------------------
+		*  COPYRIGHT YEARS
+		* -------------------------------------------------------
+	*/
+    function getCopyrightYears() {
+        var dateCopyrightHTML = document.querySelector('.js-copyright-years');
+        var date = new Date();
+        var dateYears = date.getFullYear();
+        dateCopyrightHTML.innerHTML = dateYears;
+    }
+    getCopyrightYears();
+
+
+
+	/*
+		* -------------------------------------------------------
+		*  LAZY LOAD
+		* -------------------------------------------------------
+	*/
+    var bLazy = new Blazy({
+        // Options
+        offset: 100
+    });
+
+
+    /*
+		* -------------------------------------------------------
+		*  ANIMATION SCROLL
+		* -------------------------------------------------------
+	*/
+    // animation arrow
+    var controllerProcess = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 5000
+        }
+    });
+    new ScrollMagic.Scene({
+        triggerElement: ".into__body"
+    })
+    .setClassToggle(".intro__arrow", "intro__arrow--active")
+    .addTo(controllerProcess);
+
+    // animation bg text 1
+    var controllerProcess2 = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 2000
+        }
+    });
+    new ScrollMagic.Scene({
+        triggerElement: ".into__body"
+    })
+    .setClassToggle(".subtitle__highlighted", "subtitle__highlighted--active")
+    .addTo(controllerProcess2);
+
+    // animation bg text 2
+    var controllerProcess3 = new ScrollMagic.Controller({
+        globalSceneOptions: {
+            duration: 2000
+        }
+    });
+    new ScrollMagic.Scene({
+        triggerElement: ".access"
+    })
+    .setClassToggle(".subtitle__highlighted", "subtitle__highlighted--active")
+    .addTo(controllerProcess3);
+
+
+})();
